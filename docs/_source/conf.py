104239ce63c312905bd66f63b94a80888a70dd7b
Graham Henke <grh255@nyu.edu>
2019-10-01 18:47:30 -0400

    Added ADRF logo

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -83,7 +83,7 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
-
+html_logo = 'images/adrf_horizontal_small.png'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
@@ -93,7 +93,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
